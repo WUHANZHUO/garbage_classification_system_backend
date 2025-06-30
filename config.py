16ee5b309bbf999910b99ddfb621633f088353b9
whz <wuhanzhuo2004@gmail.com>
2025-06-30 13:50:00 +0800

    修改config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 class Config:
     # 【第一步：临时注释掉这一行】
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # 【第二步：新增这一行进行测试，我们使用一个纯粹的、无歧义的字符串】
-    SECRET_KEY = 'a-super-secret-key-that-must-work-123'
+    # SECRET_KEY = 'a-super-secret-key-that-must-work-123'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
